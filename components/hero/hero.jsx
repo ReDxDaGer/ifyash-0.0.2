f9913248d0c00140723b588c9b42ea9bf24f45ad
ReDxDaGer <yashmehrotra45@gmail.com>
2024-05-04 21:10:00 +0530

    Added the hero component

---
+++ components/hero/hero.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from 'react';
+import styles from './hero.module.css';
+import myImage from '../../images/yash.jpeg';
+import {motion} from 'framer-motion'
+import Image from 'next/image'
+
+const Hero = () => {
+  return (
+    
+  );
+};
+
+export default Hero;
